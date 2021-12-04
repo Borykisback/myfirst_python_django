ef3471a688161a0e8b583a4ea22a58a6b95a5fac
Borykisback <bokback@mail.ru>
2021-12-04 23:31:46 +0300

    Add words count feature

--- mst/code.py
+++ mst/code.py
@@ -6,4 +6,5 @@ def standart(request):
 def reverse(request):
     user_text = request.GET['usertext']
     revers_text = user_text[::-1]
-    return render(request, "reverse.html", {'usertext':user_text, 'reversed':revers_text})
+    number_text = len(user_text.split())
+    return render(request, "reverse.html", {'usertext':user_text, 'reversed':revers_text, 'number':number_text})
